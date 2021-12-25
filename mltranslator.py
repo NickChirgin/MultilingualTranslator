fb92e0d0676ff0ba49ac151e90831ce904676ff7
Nikolay Chirgin <nickchirgin@gmail.com>
2021-12-25 12:08:31 +0300

    Third Commit

--- mltranslator.py
+++ mltranslator.py
@@ -8,9 +8,10 @@ def main():
     word = input("Type the word you want to translate:")
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = ""
-    ln_dict = { "en" : "english", "fr" : 'french'}
+    ln_dict = { "en" : "English", "fr": 'French'}
     result = []
     example_list = []
+    original_list = []
     if language == "en":
         url = f"https://context.reverso.net/translation/french-english/{word}"
     else:
@@ -20,13 +21,20 @@ def main():
     output(language, word)
     if r.status_code == 200:
         print(200, "OK")
-    print("Translation")
+
     for item in soup.select('#translations-content .translation'):
         result.append(item.text.strip())
     for example in soup.select('#examples-content .example .trg .text'):
         example_list.append(example.text.strip())
-    print(result)
-    print(example_list)
+    for original in soup.select('#examples-content .example .src .text, #examples-content .example .src .text-nikkud'):
+        original_list.append(original.text.strip())
+    print(f"{ln_dict.get(language)} Translations:")
+    for i in result:
+        print(i)
+    print(f"{ln_dict.get(language)} Examples:")
+    for i in range(0, 5):
+        print(original_list[i])
+        print(example_list[i])
 
 
 def output(chosen_language, chosen_word):
